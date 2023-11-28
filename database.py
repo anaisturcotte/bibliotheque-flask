e4742cade174e7bc2bab69bf8bdd93b2b5cb76b4
NikolaEllis <143142336+NikolaEllis@users.noreply.github.com>
2023-11-28 14:11:11 -0500

    ajout de la mise en page sur toutes les pages

--- database.py
+++ database.py
@@ -15,23 +15,27 @@ def _select(requete, params=None):
 
 
 def get_item_by(id_Createur):
-    requete = """select Item.titre, Item.anneeSortie, Item.description
+    requete = """select Item.titre, Item.anneeSortie
                     from Item
                     where Item.idCreateur=?
                     order by Item.annee desc"""
     return _select(requete, params=(id_Createur))
 
 def get_all_items():
-    requete = """select Item.image, Item.titre, Item.anneeSortie, Item.description Type.nomType
+    requete = """select Item.image, Item.titre, Item.anneeSortie, Type.nomType
                         from Item inner join type on Item.idType=Type.id"""
     return _select(requete)
 
 def get_all_films():
-    requete = """select titre, anneeSortie, description from Item inner join type on Item.idType=Type.id where Type.nomType = 'Film'"""
+    requete = """select Item.image, Item.titre, Item.anneeSortie, Type.nomType 
+                        from Item inner join type on Item.idType=Type.id 
+                        where Type.nomType = 'Film'"""
     return _select(requete)
 
 def get_all_livre():
-    requete = """select titre, anneeSortie, description from Item inner join type on Item.idType=Type.id where Type.nomType = 'Livre'"""
+    requete = """select Item.image, Item.titre, Item.anneeSortie, Type.nomType 
+                        from Item inner join type on Item.idType=Type.id 
+                        where Type.nomType = 'Livre'"""
     return _select(requete)
 
 def get_all_musiques():
